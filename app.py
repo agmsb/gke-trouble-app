7e119caf0cd2dd93e8f3c6abd10e19697b86d919
anthony bushong <agmsb@users.noreply.github.com>
2021-08-12 09:25:12 -0700

    Add Python app.

---
+++ app.py
@@ -0,0 +1,19 @@
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/healthz')
+def healthz():
+    resp = Response("OK", status=200)
+    return resp
+
+@app.route('/crashme')
+def crash_me():
+    if os.environ.get('CAN_CRASH') == "True":
+        os._exit(0)
+    else:
+        resp = Response("Can't crash!", status=200)
+        return resp
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
